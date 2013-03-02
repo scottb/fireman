27ea2b9e904b5edc18811898a4e6cc425fb924ec
Scott Brickner <scottb@brickner.net>
2013-03-02 16:19:42 -0500

    Configure devise and friends

--- Gemfile
+++ Gemfile
@@ -2,39 +2,13 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
-
-
-# Gems used only for assets and not required
-# in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
 
 gem "mysql2", ">= 0.3.11"
 gem "rspec-rails", ">= 2.12.2", :group => [:development, :test]
@@ -50,3 +24,10 @@ gem "devise_invitable", ">= 1.1.5"
 gem "cancan", ">= 1.6.8"
 gem "rolify", ">= 3.2.0"
 gem "simple_form", ">= 2.0.4"
+gem "quiet_assets", ">= 1.0.1", :group => :development
+gem "figaro", ">= 0.5.3"
+gem "better_errors", ">= 0.6.0", :group => :development
+gem "binding_of_caller", ">= 0.7.1", :group => :development, :platforms => [:mri_19, :rbx]
+gem "libv8", ">= 3.11.8"
+gem "therubyracer", ">= 0.11.3", :group => :assets, :platform => :ruby, :require => "v8"
+gem "hub", ">= 1.10.2", :require => nil, :group => [:development]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Fireman::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +50,27 @@ Fireman::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'static#home'
 end
+=begin
+        new_user_session GET    /users/sign_in(.:format)          devise/sessions#new
+            user_session POST   /users/sign_in(.:format)          devise/sessions#create
+    destroy_user_session DELETE /users/sign_out(.:format)         devise/sessions#destroy
+           user_password POST   /users/password(.:format)         devise/passwords#create
+       new_user_password GET    /users/password/new(.:format)     devise/passwords#new
+      edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
+                         PUT    /users/password(.:format)         devise/passwords#update
+cancel_user_registration GET    /users/cancel(.:format)           devise_invitable/registrations#cancel
+       user_registration POST   /users(.:format)                  devise_invitable/registrations#create
+   new_user_registration GET    /users/sign_up(.:format)          devise_invitable/registrations#new
+  edit_user_registration GET    /users/edit(.:format)             devise_invitable/registrations#edit
+                         PUT    /users(.:format)                  devise_invitable/registrations#update
+                         DELETE /users(.:format)                  devise_invitable/registrations#destroy
+       user_confirmation POST   /users/confirmation(.:format)     devise/confirmations#create
+   new_user_confirmation GET    /users/confirmation/new(.:format) devise/confirmations#new
+                         GET    /users/confirmation(.:format)     devise/confirmations#show
+             user_unlock POST   /users/unlock(.:format)           devise/unlocks#create
+         new_user_unlock GET    /users/unlock/new(.:format)       devise/unlocks#new
+                         GET    /users/unlock(.:format)           devise/unlocks#show
+                    root        /                                 static#home
+=end
