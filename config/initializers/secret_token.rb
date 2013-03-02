dd8311403eb6e370b26987f73896a0b21074f2de
Scott Brickner <scottb@brickner.net>
2013-03-02 15:26:58 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fireman::Application.config.secret_token = '7c857a6a28d3ed39c0c115f27d10ec1a398345a3b5c5f0048a3cb14c35537e963ccc4a7a8041dec2b400745265a80c91ab64c371be388f6cf745056e2b86e205'
